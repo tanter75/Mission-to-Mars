1ecfab78776588b01675e33184f4900a0eb137a0
tanter75 <86161212+tanter75@users.noreply.github.com>
2021-09-11 18:27:03 -0400

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -97,9 +97,7 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html(classes="table table-striped")
-
-
+    return df.to_html(classes="table table-striped table-dark")
 def hemispheres(browser):
     url = 'https://marshemispheres.com/'
 
